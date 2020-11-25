50376f4b1dac1eef2ba71f752f482e225fa8733e
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-11-26 03:39:07 +0600

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Test;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function test() {
+//        Test::create([
+//            'name' => 'Uraaa'
+//        ]);
+        $tests = Test::all();
+        dd($tests);
+        return view('home');
+    }
+}
---
+++ app/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Test extends Model
+{
+    protected $connection = 'mongodb';
+
+    protected $collection = 'tests';
+    protected $fillable = ['name'];
+
+
+}
